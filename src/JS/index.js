5609eacb0412d16198077eea9ddda1dafe487e92
garciapaulo95 <garciapaulo1995@gmail.com>
2023-02-18 00:07:45 -0300

    add: files and base project

---
+++ src/JS/index.js
@@ -0,0 +1,6 @@
+// Requisição API
+// http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=595f7ca9f8d674919076afef5646f19a
+
+$(function() {
+    var marvelApi = 'http://gateway.marvel.com/v1/public/comics'
+})
